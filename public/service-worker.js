5ef4574d8842e5a4722b0c4777353b4b1ee6c67c
Sonika <sonikapoudyal5@gmail.com>
2021-08-26 12:45:03 -0400

    fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,20 +3,30 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/style.css",
-    "./js/index.js",
-    "./js/idb.js",
-    "./manifest.json",
-    "./icons/icon-512x512.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-72x72.png"
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/js/index.js",
+    "/js/idb.js",
+    "/manifest.json",
+    "/icons/icon-512x512.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-72x72.png"
 ];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE).catch(err => console.log(err))
+        })
+    )
+})
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
@@ -29,15 +39,6 @@ self.addEventListener('fetch', function (e) {
                 console.log('file is not cached, fetching : ' + e.request.url)
                 return fetch(e.request)
             }
-        })
-    )
-})
-
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
         })
     )
 })
